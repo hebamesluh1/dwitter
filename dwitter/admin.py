ec80d1849b61dd047743250952b33eaefe3140b5
hebamesluh1 <hebamesluh@gmail.com>
2024-12-17 14:34:25 +0200

    create a model and database

---
+++ dwitter/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.contrib.auth.models import User, Group
+
+
+
+# dwitter/admin.py
+# ...
+from .models import Profile
+# ...
+admin.site.register(Profile)
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
+
+
+
