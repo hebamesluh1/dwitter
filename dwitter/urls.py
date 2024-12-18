fc5cfe996d268606c8f8ab6debd3d14a838785e4
hebamesluh1 <hebamesluh@gmail.com>
2024-12-18 14:58:47 +0200

    add profile list url

--- dwitter/urls.py
+++ dwitter/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import dashboard
-
+from .views import dashboard, profile_list
 app_name = "dwitter"
+
 urlpatterns = [
 path("", dashboard, name="dashboard"),
+path("profile_list/", profile_list, name="profile_list"),
 ]
