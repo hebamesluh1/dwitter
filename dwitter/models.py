447f5756fb618d9e90688608545d3520ea988e7c
hebamesluh1 <hebamesluh@gmail.com>
2024-12-23 12:04:14 +0200

    fixed issues in profile view

--- dwitter/models.py
+++ dwitter/models.py
@@ -8,7 +8,10 @@ from django.dispatch import receiver
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     follows = models.ManyToManyField(
-"self", related_name="followed_by", symmetrical=False, blank=True
+"self", 
+related_name="followed_by", 
+symmetrical=False, 
+blank=True
 )
     def __str__(self):
         return self.user.username
