e58a3bd32e952989a79e0b013f2d315ae9c4b0ae
hebamesluh1 <hebamesluh@gmail.com>
2024-12-24 13:56:02 +0200

    fixed post request with redirect

--- dwitter/views.py
+++ dwitter/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import DweetForm
 from .models import Profile
 
 def dashboard(request):
     if request.method == "POST":
-        form = DweetForm(request.POST)
+        form = DweetForm(request.POST or None)
         if form.is_valid():
             dweet = form.save(commit=False)
             dweet.user = request.user
             dweet.save()
-        form = DweetForm()
+            return redirect("dwitter:dashboard")
+    form = DweetForm()
     return render(request, "dwitter/dashboard.html", {"form": form})
 
 
